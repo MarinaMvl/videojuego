690ab1298d094c7a0a272a3fb8112cc2f9ee9aef
Maryna Ulasava <marynaulasava@gmail.com>
2020-03-08 22:14:03 +0100

    versión refactorizada

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -2,6 +2,13 @@ package net.instituto.desa;
 
 public class Jugador {
 	int y, x;
+	private static final String ABAJO = "Abajo";
+	private static final String ARRIBA = "Arriba";
+	private static final String IZQUIERDA = "Izquierda";
+	private static final String DERECHA = "Derecha";
+
+	
+	
 
 	public int getY() {
 		return y;
@@ -18,5 +25,23 @@ public class Jugador {
 	public void setX(int x) {
 		this.x = x;
 	}
-
+	
+	public void moverDerecha(String m){
+	if (m.equalsIgnoreCase(DERECHA)) 
+		setX(getX() + 1);
+	}
+	
+	public void moverIzquierda(String m){
+		if (m.equalsIgnoreCase(IZQUIERDA)) 
+			setX(getX() - 1);
+		}
+	public void moverArriba(String m){
+		if (m.equalsIgnoreCase(ARRIBA)) 
+			setY(getY() - 1);
+		}
+	public void moverAbajo(String m){
+		if (m.equalsIgnoreCase(ABAJO)) 
+			setY(getY() + 1);
+		}
+		
 }
